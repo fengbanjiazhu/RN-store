9c8d85edd68b04ffd2344a4f95ba7a924bed37e8
Jeffrey <562475805lanu@gmail.com>
2023-09-19 17:13:58 +1000

    add init screen and dependencies

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserProductsScreen() {
+  return <div>UserProductsScreen</div>;
+}
+
+export default UserProductsScreen;
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductsOverviewScreen() {
+  return <div>ProductsOverviewScreen</div>;
+}
+
+export default ProductsOverviewScreen;
