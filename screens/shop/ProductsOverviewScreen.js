4cc280ba728dc0111df522465c836f3682fa40dc
Jeffrey <562475805lanu@gmail.com>
2023-09-21 20:41:52 +1000

    render product list

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { FlatList } from "native-base";
 
+import ProductList from "../../components/ProductList";
+
 function ProductsOverviewScreen({}) {
-  return <FlatList>ProductsOverviewScreen</FlatList>;
+  const productData = useSelector((state) => state.product.availableProducts);
+
+  return (
+    <FlatList
+      data={productData}
+      renderItem={({ item }) => <ProductList product={item} />}
+      keyExtractor={(item) => item._id}
+    />
+  );
 }
 
 export default ProductsOverviewScreen;
--- service/apiProducts.js
+++ service/apiProducts.js
@@ -3,9 +3,11 @@ const API_URL = "http://localhost:4000/api/";
 export async function fetchProduct() {
   try {
     const res = await fetch(`${API_URL}products`);
-    const data = await res.json();
 
-    console.log(data);
+    if (!res.ok) throw Error("Failed getting products");
+
+    const { data } = await res.json();
+    return data;
   } catch (error) {
     console.log(error);
   }
